8a3ee71ae104128c65e2a77d5259f8e2709017a1
MarcDrudis <MarcSanzDrudis@outlook.com>
2023-02-08 17:22:25 +0100

    asdffdf

--- gibbs_code/setup.py
+++ gibbs_code/setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=["qiskit-terra == 0.23.0rc1",
                       "qiskit-aer",
                       "qiskit-ibm-runtime",
-                      'pip install qiskit_ibmq_provider'
+                      'qiskit_ibmq_provider'
                       ],
     dependency_links=["git+"],
 )
